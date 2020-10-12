0369344849b05c4cbdd86cc4d57e99fae4b9f660
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-12 14:22:49 +0300

    cors issue

--- config/application.rb
+++ config/application.rb
@@ -33,16 +33,5 @@ module MicroverseJobsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.middleware.insert_before ActionDispatch::Static, "Rack::Cors" do
-      allow do
-        origins '*'
-        resource(
-          '*',
-          headers: :any,
-          methods: :any
-          )
-      end
-    end
   end
 end
