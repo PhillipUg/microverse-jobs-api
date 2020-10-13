9e8fcc0ec86c8ec94272dbf4e528b7fc3a6b515e
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-13 16:29:16 +0300

    create migration for user table and run migration to create schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_083116) do
+ActiveRecord::Schema.define(version: 2020_10_13_125506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2020_10_12_083116) do
     t.string "company"
     t.string "position"
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
