f46a77a60f885ba04b008597de65d44e2d884da0
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-13 16:28:03 +0300

    add routes for login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
+  # get '/users', to: 'users#index'
+  # get "/auto_login", to: "users#auto_login"
+  
   namespace :api do
     namespace :v1 do
-      resources :jobs
+      resources :users, only: [:index, :create]
+      post '/login', to: "users#login"
+      resources :jobs, only: [:index, :create, :show, :destroy, :update]
     end
   end
 end
