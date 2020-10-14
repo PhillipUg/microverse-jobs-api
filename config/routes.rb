96cf12760f0f1e53e58bc507a981dd1948f69111
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-14 16:38:22 +0300

    add route to user jobs

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       resources :users, only: [:index, :create]
       resources :favorites, only: [:index, :update]
       post '/login', to: "users#login"
+      get '/user-jobs', to: "users#user_jobs"
       resources :jobs, only: [:index, :create, :show, :destroy, :update]
     end
   end
