8b48a4b9abad8eb7f688ee4d42e32be550c5dad4
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-14 12:50:41 +0300

    add index action in favorite controller. refactored create and destroy actions into one update action

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,21 +1,22 @@
 class Api::V1::FavoritesController < ApplicationController
 
-  def create
-    @favorite = logged_in_user.favorites.new(job_id: params[:job_id])
-
-    if @favorite.save
-      @favorite_exists = true
-      render json: {favorited: @favorite_exists, message: "Added to favorites" }
-    end
+  def index
+    favorites = Favorite.all
+    render json: {favorites: favorites}
   end
 
-  def destroy
+  def update
     favorite = Favorite.find_by(user: logged_in_user, job_id: params[:job_id])
-
+    
     if favorite
       @favorite_exists = false
-      render json: {favorited: @favorite_exists, message: "Removed from favorites" }
       favorite.destroy
+      render json: {favorite: favorite, favorited: @favorite_exists, message: "Removed from favorites" }
+    else
+      favorite = Favorite.create(user: logged_in_user, job_id: params[:job_id])
+      @favorite_exists = true
+      render json: {favorite: favorite, favorited: @favorite_exists, message: "Added to favorites" }
     end
   end
+
 end
