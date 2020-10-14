51c13b5f4bf70a12b92359c6195c226b68ce9c0d
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-14 07:11:37 +0300

    add favorites create and destroy routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :create]
+      resource :favorites, only: [:create, :destroy]
       post '/login', to: "users#login"
       resources :jobs, only: [:index, :create, :show, :destroy, :update]
     end
