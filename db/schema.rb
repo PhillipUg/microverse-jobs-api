c01c24478c372a44f7ee2917bdc272fae406a766
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-16 17:26:16 +0300

    add salary and location fields to jobs table. add seeds for database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_030514) do
+ActiveRecord::Schema.define(version: 2020_10_16_140355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2020_10_14_030514) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "salary"
+    t.string "location"
   end
 
   create_table "users", force: :cascade do |t|
