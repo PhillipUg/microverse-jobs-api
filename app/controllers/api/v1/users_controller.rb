3d9e8d662bc315b80fad384ea678bb4cc9edc969
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-20 21:05:56 +0300

    remove auto_login action and redundant age param

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -34,13 +34,9 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
-  def auto_login
-    render json: @user
-  end
-
   private
 
   def user_params
-    params.permit(:username, :password, :age)
+    params.permit(:username, :password)
   end
 end
